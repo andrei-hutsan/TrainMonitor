0825c3a93f09fc28d563a8e724b61e22f95cbe80
andreih <andrei.hutsan@mindsoft.ro>
2025-09-30 14:18:30 +0300

    Load the project

---
+++ TrainMonitor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrainMonitor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
