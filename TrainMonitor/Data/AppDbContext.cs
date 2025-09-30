6fcb3c86bba27bbbedafa2f4a9989a7d30a8740a
andreih <andrei.hutsan@mindsoft.ro>
2025-09-30 14:24:55 +0300

    Add DbContext

---
+++ TrainMonitor/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainMonitor.Models;
+
+namespace TrainMonitor.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        public DbSet<Train> Trains { get; set; }
+        public DbSet<Incident> Incidents { get; set; }
+    }
+}
--- TrainMonitor/Program.cs
+++ TrainMonitor/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TrainMonitor.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,5 +31,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
