e0e1a2e78848446068d3af3f261e2ccb302adabd
andreih <andrei.hutsan@mindsoft.ro>
2025-09-30 14:21:42 +0300

    Add models

---
+++ TrainMonitor/Models/Incident.cs
@@ -0,0 +1,15 @@
+﻿namespace TrainMonitor.Models
+{
+    public class Incident
+    {
+        public Guid Id { get; set; }
+        public Guid TrainId { get; set; }
+        public string Username { get; set; }
+        public string Reason { get; set; }
+        public string Comment { get; set; }
+        public DateTime Timestamp { get; set; }
+
+
+        public Train Train { get; set; }
+    }
+}
---
+++ TrainMonitor/Models/Train.cs
@@ -0,0 +1,15 @@
+﻿namespace TrainMonitor.Models
+{
+    public class Train
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public int? DelayMinutes { get; set; }
+        public string NextStop { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+
+        public ICollection<Incident> Incidents { get; set; }
+    }
+}
