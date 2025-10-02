3a97327e139933a2ab5827faaa24887131e1a012
andreih <andrei.hutsan@mindsoft.ro>
2025-10-02 15:11:14 +0300

    Adjust UI/UX for better user felling

--- TrainMonitor/Models/Incident.cs
+++ TrainMonitor/Models/Incident.cs
@@ -1,15 +1,22 @@
-﻿namespace TrainMonitor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainMonitor.Models
 {
     public class Incident
     {
         public Guid Id { get; set; }
-        public Guid TrainId { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Reason is required")]
         public string Reason { get; set; }
-        public string? Comment { get; set; }
+
+        [Required(ErrorMessage = "Comment is required")]
+        public string Comment { get; set; }
+
+        public Guid TrainId { get; set; }
         public DateTime Timestamp { get; set; }
-
-
-        public Train? Train { get; set; }
     }
 }
+
